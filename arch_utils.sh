c65f302e50ecc6f74fbfe2c1bf402166bc59ae9d
Utkarsh Shekhar <iamutkarshshekhar@gmail.com>
2025-09-22 01:26:05 +0530

    Add yt-dlp package to install

--- arch_utils.sh
+++ arch_utils.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S base-devel git neovim less man-db man-pages which file tree zip unzip wget curl jq htop ncdu ripgrep fd bat exa openssh net-tools inetutils dnsutils iproute2 iputils sudo bash-completion python go rust nodejs npm
+sudo pacman -S base-devel git neovim less man-db man-pages which file tree zip unzip wget curl jq htop ncdu ripgrep fd bat exa openssh net-tools inetutils dnsutils iproute2 iputils sudo bash-completion python go rust nodejs npm yt-dlp
